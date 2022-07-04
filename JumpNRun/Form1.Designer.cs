690440f6bfbcdad9777e43b1f8a9dc13759688ab
Gaming Intel <Gaming Intel@DESKTOP-SEK3G2F>
2022-07-04 20:08:53 +0200

    Add project files.

---
+++ JumpNRun/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Dodger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbRunner = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRunner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbRunner
+            // 
+            this.pbRunner.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbRunner.Location = new System.Drawing.Point(348, 302);
+            this.pbRunner.Name = "pbRunner";
+            this.pbRunner.Size = new System.Drawing.Size(50, 50);
+            this.pbRunner.TabIndex = 0;
+            this.pbRunner.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbRunner);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRunner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbRunner;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ JumpNRun/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            createPictureBox();
+            
+        }
+
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<Color> colors = new List<Color>()
+            {
+                Color.Red,
+                Color.Orange,
+                Color.Yellow,
+                Color.Green,
+                Color.Blue,
+                Color.Indigo,
+                Color.Violet
+            };
+        private int counter = 0;
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            switch (e.KeyChar)
+            {
+                case 'a':
+
+                    if (pbRunner.Location.X + pbRunner.Width < 0)
+                    {
+                        pbRunner.Location = new Point(Width - pbRunner.Width, pbRunner.Location.Y);
+                    }
+                    else
+                    {
+                        pbRunner.Location = new Point(pbRunner.Location.X - 15, pbRunner.Location.Y);
+                    }
+
+                    break;
+
+                case 'd':
+
+                    if (pbRunner.Location.X > Width)
+                    {
+                        pbRunner.Location = new Point(0, pbRunner.Location.Y);
+                    }
+                    else
+                    {
+                        pbRunner.Location = new Point(pbRunner.Location.X + 15, pbRunner.Location.Y);
+                    }
+                                        
+                    break;
+
+                    //case 'w':
+                    //    pbRunner.Location = new Point(pbRunner.Location.X, pbRunner.Location.Y - 15);
+                    //    break;
+                    //case 's':
+                    //    pbRunner.Location = new Point(pbRunner.Location.X, pbRunner.Location.Y + 15);
+                    //    break;
+
+            }
+
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //pictureBoxes.Add(createPictureBox());
+
+            //foreach (var item in pictureBoxes)
+            //{
+            //    item.Location = new Point(40, item.Location.Y + 5);
+            //}
+
+            
+            pictureBoxes[0].Location = new Point(pictureBoxes[0].Location.X, pictureBoxes[0].Location.Y + 5);
+
+            foreach (var picturebox in pictureBoxes)
+            {
+                if (picturebox.Bounds.IntersectsWith(pbRunner.Bounds))
+                {
+                    timer1.Stop();
+                    DialogResult result = MessageBox.Show("Leider verloren :(\nWillst du es nochmal versuchen?", "Verloren", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result.Equals(DialogResult.Yes))
+                    {
+                        Application.Restart();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+
+                }
+            }
+
+            if (pictureBoxes[0].Location.Y > Height)
+            {
+                this.Controls.Remove(pictureBoxes[0]);
+                pictureBoxes.RemoveAt(0);
+                createPictureBox();
+            }
+        }
+
+        public PictureBox createPictureBox()
+        {
+            Random random = new Random();
+            int randX = random.Next(0, Width);
+
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Size = new Size(5, 10);
+            pictureBox.BackColor = Color.Red;
+
+            counter++;
+            if (counter == 5)
+            {
+                int xPos = pbRunner.Location.X + (pbRunner.Width / 2);
+                pictureBox.Location = new Point(xPos, 0);
+                counter = 0;
+            }
+            else
+            {
+                pictureBox.Location = new Point(randX, 0);
+            }
+            
+            pictureBoxes.Add(pictureBox);
+            this.Controls.Add(pictureBox);
+
+            return pictureBox;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int newRand = rand.Next(colors.Count);
+
+            pbRunner.BackColor = colors[newRand];
+
+        }
+    }
+}
